688fa424c03a91d6604a4d13c7e6682ab09cf869
Alex Strandberg <8642246+alexstrandberg@users.noreply.github.com>
2016-06-14 14:35:17 -0700

    Assignment 1 Uploaded
    tippy is a tip calculator application for iOS.

--- tippy/ViewController.swift
+++ tippy/ViewController.swift
@@ -9,6 +9,10 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var billField: UITextField!
+    @IBOutlet weak var tipLabel: UILabel!
+    @IBOutlet weak var totalLabel: UILabel!
+    @IBOutlet weak var tipControl: UISegmentedControl!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,6 +24,17 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func onTap(sender: AnyObject) {
+        view.endEditing(true)
+    }
 
+    @IBAction func calculateTip(sender: AnyObject) {
+        let bill = Double(billField.text!) ?? 0
+        let tipPercentages = [0.18, 0.20, 0.25]
+        let tip = bill * tipPercentages[tipControl.selectedSegmentIndex]
+        let total = bill + tip
+        
+        tipLabel.text = String(format: "$%.2f", tip)
+        totalLabel.text = String(format: "$%.2f", total)
+    }
 }
-
